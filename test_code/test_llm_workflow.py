6b504e99cf2a4bd645d4a589a9b2889eda6e9f28
Vijay Dipak Takbhate <vijaytakbhate20@gmail.com>
2026-03-04 00:19:40 +0530

    Updated Pytest Code

--- test_code/test_llm_workflow.py
+++ test_code/test_llm_workflow.py
@@ -5,7 +5,7 @@ import os
 # Mock constants if they are not available from the package directly or just use literals for tests
 GPT_MODEL_NAME = "gpt-5-mini"
 TEMPERATURE = 0.5
-VECTORDB_PATH = "./vector_db"
+VECTORDB_PATH = "./github_vector_db"
 COLLECTION_NAME = "my_embeddings"
 
 @pytest.fixture(scope="module")
