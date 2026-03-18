8e013328a75880ca28ebdfb327baa456dbf23173
Vijay Dipak Takbhate <vijaytakbhate20@gmail.com>
2026-03-18 19:59:21 +0530

    Fix app.run command

--- app.py
+++ app.py
@@ -206,10 +206,5 @@ def end_chat():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000, debug=False)
-    port = int(os.environ.get("PORT"))  # do NOT provide a default
-    if port:
-        app.run(host="0.0.0.0", port=port, debug=False)
-    else:
-        app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
